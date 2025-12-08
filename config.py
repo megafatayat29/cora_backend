2877fc7a71218c5738797ff6615dd300f4df2f5f
addpra90 <liedyprasetyo@gmail.com>
2025-12-08 22:16:07 +0700

    add chatbot

--- config.py
+++ config.py
@@ -1,13 +1,17 @@
 from functools import lru_cache
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     NEO4J_URI: str = "neo4j://mentorku.cloud:7687"
     NEO4J_USER: str = "neo4j"
     NEO4J_PASSWORD: str = "xsw21qaz"
 
-    class Config:
-        env_file = ".env"
+    HF_TOKEN: str | None = None
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore",  # agar tidak error jika ada variable lain
+    )
 
 @lru_cache()
 def get_settings():
